5082212ce357192a183bc85251f9880d0e818ed0
Turpal <voin_957@mail.ru>
2024-10-01 11:59:34 +0300

    feat: finish game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.med = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.med = Math.round((this.min + this.max) / 2);
+  }
+  
+  guess() {
+    return this.med;
+  }
 
-    }
+  lower() {
+    this.max = this.med;
+    this.med = Math.round((this.min + this.max) / 2);
+    return this.med;
+  }
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    let cur = this.root;
+    this.min = this.med;
+    this.med = Math.round((this.min + this.max) / 2);
+    return this.med;
+  }
 }
 
 module.exports = GuessingGame;
